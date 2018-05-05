05b0ca94615eabb211290599df3f2a737265c0f3
HBJ <bomul0327@naver.com>
2018-05-05 22:29:00 +0900

    Added Main Stage features

---
+++ Assets/02. Scripts/StartUI/UIBlink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBlink : MonoBehaviour {
+	public float duration = 1f;
+	MaskableGraphic uiGraphic;
+	void Awake(){
+		uiGraphic = GetComponent<MaskableGraphic>();
+	}
+
+	void Start(){
+		StartCoroutine(Blink());
+	}
+
+	IEnumerator Blink(){
+
+		while(true){
+			uiGraphic.CrossFadeAlpha(0, duration, true);
+			yield return new WaitForSecondsRealtime(duration);
+			Debug.Log(uiGraphic.color);
+			uiGraphic.CrossFadeAlpha(1, duration, true);
+			yield return new WaitForSecondsRealtime(duration);
+		}
+	}
+	
+	void OnEnable()
+	{
+		uiGraphic.color = Color.white;
+	}
+}
