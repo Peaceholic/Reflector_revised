e2e00dc9fd72a5ec6bcdaedf401c858c2e4b0211
Jungs <jeongy95@naver.com>
2018-05-05 22:31:55 +0900

    shield attack developed

---
+++ Assets/02. Scripts/ShieldAttack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShieldAttack : MonoBehaviour, IPointerClickHandler {
+
+	public RectTransform canvas;
+	public GameObject bulletPrefab;
+	public GameObject shield;
+	public float attackSpeed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	public void OnPointerClick(PointerEventData eventData) {
+		GameObject bulletObject = Instantiate(bulletPrefab, shield.transform.position, Quaternion.identity);
+		Vector2 attackDir = -(shield.transform.parent.parent.position - shield.transform.position);
+		attackDir.Normalize();
+		bulletObject.GetComponent<Rigidbody2D>().velocity = attackDir * attackSpeed;
+	}
+}
