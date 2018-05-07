9304104bcfb863b29ec70cbc78a64f208c9b7475
Jungs <jeongy95@naver.com>
2018-05-07 15:26:12 +0900

    shooter random spawn develop

---
+++ Assets/02. Scripts/GameMgr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour {
+
+	public int spawnTime = 7;
+
+	private ShooterSpawnEffect shooterSpawn;
+
+	// Use this for initialization
+	void Start () {
+		shooterSpawn = GetComponent<ShooterSpawnEffect>();
+		StartCoroutine(StartSpawn());
+	}
+	
+	IEnumerator StartSpawn() {
+		
+		while(true) {
+			shooterSpawn.RandomSpawn();
+
+			yield return new WaitForSeconds(spawnTime);
+		}
+	}
+}
---
+++ Assets/02. Scripts/PlayerCtrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+	public int health = 2;
+
+    void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "EnemyBullet") {
+			health--;
+		}
+	}
+
+}
