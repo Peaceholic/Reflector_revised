9d49ff010d0792e708f803fc4abdd5f2da074eed
HBJ <bomul0327@naver.com>
2018-05-11 17:10:05 +0900

    Edited PlayerCtrl

--- Assets/02. Scripts/PlayerCtrl.cs
+++ Assets/02. Scripts/PlayerCtrl.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour {
+	private int currentHealth;
+	public int maxHealth = 2;
 
-	public int health = 2;
+	void Start()
+	{
+		ResetStatus();
+	}
 
     void OnTriggerEnter2D(Collider2D other) {
-		if(other.gameObject.tag == "EnemyBullet") {
-			health--;
+		if(other.gameObject.CompareTag("EnemyBullet")) {
+			ReceiveDamage(1);
 		}
 	}
 
+	void ResetStatus(){
+		currentHealth = maxHealth;
+	}
+
+	void ReceiveDamage(int amount){
+		currentHealth -= 1;
+		if(currentHealth <= 0){
+			Die();
+		}
+	}
+
+	void Die(){
+		//Player death
+	}
 }
