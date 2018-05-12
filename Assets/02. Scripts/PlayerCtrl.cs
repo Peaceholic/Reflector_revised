894cdc821c304579677262e12f4670fead82f846
HBJ <bomul0327@naver.com>
2018-05-12 21:32:32 +0900

    Removed PlayerCtrlWithJoystick and ShieldCtrlWithJoystick Edited PlayerCtrl and ShieldCtrl to control character and shield
    Changed isTrigger to be true

--- Assets/02. Scripts/PlayerCtrl.cs
+++ Assets/02. Scripts/PlayerCtrl.cs
@@ -6,9 +6,19 @@ public class PlayerCtrl : MonoBehaviour {
 	private int currentHealth;
 	public int maxHealth = 2;
 
+	public float moveSpeed = 5.0f;
+
+	private JoystickPlayer joystick;
+
 	void Start()
 	{
+		joystick = FindObjectOfType<JoystickPlayer>();
 		ResetStatus();
+	}
+
+	void Update()
+	{
+		Move();
 	}
 
     void OnTriggerEnter2D(Collider2D other) {
@@ -16,7 +26,19 @@ public class PlayerCtrl : MonoBehaviour {
 			ReceiveDamage(1);
 		}
 	}
+	void Move() {
+		Vector2 moveDir = new Vector2(joystick.Horizontal, joystick.Vertical);
 
+		transform.Translate(moveDir * Time.deltaTime * moveSpeed, Space.Self);
+        /*
+        horizontal = Input.GetAxisRaw("Horizontal");
+		vertical = Input.GetAxisRaw("Vertical");
+
+		Vector3 moveDir = (Vector3.up * vertical) + (Vector3.right * horizontal);
+
+        transform.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
+        */
+	}
 	void ResetStatus(){
 		currentHealth = maxHealth;
 	}
