844efd34783ae411550a13d44b6014947049a392
HBJ <bomul0327@naver.com>
2018-06-23 15:48:47 +0900

    Separated shield logic from shield UI

--- Assets/02. Scripts/ShieldAttack.cs
+++ Assets/02. Scripts/ShieldAttack.cs
@@ -4,15 +4,15 @@ using UnityEngine.EventSystems;
 public class ShieldAttack : MonoBehaviour, IPointerClickHandler {
 
 	public RectTransform canvas;
-	public GameObject bulletPrefab;
-	public GameObject shield;
-	public GameObject DieEffect;
-	public float attackSpeed = 20.0f;
+	private ShieldCtrl shield;
+
+	void Start()
+	{
+		shield = FindObjectOfType<ShieldCtrl>();
+	}
 	
 	public void OnPointerClick(PointerEventData eventData) {
-		GameObject bulletObject = Instantiate(bulletPrefab, shield.transform.position, Quaternion.identity);
-		Vector2 attackDir = -(shield.transform.parent.parent.position - shield.transform.position);
-		attackDir.Normalize();
-		bulletObject.GetComponent<Rigidbody2D>().velocity = attackDir * attackSpeed;
+		shield.Attack();
+		
 	}
 }
