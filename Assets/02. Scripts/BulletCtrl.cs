74e62ba3ebef9c9f21ab5cac3ec5bf0005dd85cb
HBJ <bomul0327@naver.com>
2018-06-23 17:05:15 +0900

    Added score addition when player kills enemy

--- Assets/02. Scripts/BulletCtrl.cs
+++ Assets/02. Scripts/BulletCtrl.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class BulletCtrl : MonoBehaviour {
 
+	public int scoreAmount = 200;
     public GameObject DieEffect;
 
     void OnTriggerEnter2D(Collider2D other) {
 		if((other.gameObject.tag == "Shooter") || (other.gameObject.tag == "Charger")) {
+			GameMgr.Instance.CurrentScore += scoreAmount;
 			Transform otherTr = other.transform;
 			Destroy(other.gameObject);
 			Instantiate(DieEffect, otherTr.position, Quaternion.identity);
