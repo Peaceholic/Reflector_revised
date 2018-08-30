2f3064e403b48a8ef1f2d88cb844abb2ad71d171
Jjungs7 <jeongy95@naver.com>
2018-08-30 10:25:47 +0900

    better control on shield joystick

--- Assets/02. Scripts/FixedJoystickShield.cs
+++ Assets/02. Scripts/FixedJoystickShield.cs
@@ -8,7 +8,7 @@ public class FixedJoystickShield : JoystickShield {
     private Vector2 joystickPosition = Vector2.zero;
     private Camera cam = new Camera();
     private bool firstTouch = true;
-    private float magnitudeMultiplier = 2.5f;
+    private float magnitudeMultiplier = 0.3f;
 
     void Start() {
         joystickPosition = RectTransformUtility.WorldToScreenPoint(cam, background.position);
